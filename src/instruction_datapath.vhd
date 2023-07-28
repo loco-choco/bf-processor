05ca227161ba5c6ce77259c330ffb5d94ff92dc2
locochoco <ivanrwpf@gmail.com>
2023-07-28 15:21:38 -0300

    changed jumping_dp to work for foward and backward

--- src/instruction_datapath.vhd
+++ src/instruction_datapath.vhd
@@ -5,10 +5,8 @@ entity instruction_datapath is
 	generic(pc_width: integer := 32);
 	port(Clk: in STD_LOGIC;
 			Reset: in STD_LOGIC;
-			-- pc override
-			Override: in STD_LOGIC;
-			PCOverride: in STD_LOGIC_VECTOR(pc_width - 1 downto 0);
-			--
+			Direction: in STD_LOGIC;
+			PCEnable: in STD_LOGIC;
 			-- instr regbank here
 			InstrPC: out STD_LOGIC_VECTOR(pc_width - 1 downto 0);
 			Instr: in STD_LOGIC_VECTOR(7 downto 0);
@@ -40,11 +38,6 @@ architecture synth of instruction_datapath is
 			Selection: in STD_LOGIC; -- 0-> addition, 1 -> subtraction
 			Output: out STD_LOGIC_VECTOR(width - 1 downto 0));
 	end component;
-	component mux2	generic(width : integer);
-	port(Input0, Input1: in STD_LOGIC_VECTOR(width - 1 downto 0);
-			Selection: in STD_LOGIC;
-			Output: out STD_LOGIC_VECTOR(width - 1 downto 0));
-	end component;
 	component instrdecoder 
 	port(Instr: in STD_LOGIC_VECTOR(7 downto 0);
 			Action: out STD_LOGIC;
@@ -52,13 +45,12 @@ architecture synth of instruction_datapath is
 			Operation: out STD_LOGIC);
 	end component;
 	
-	signal PC, NextPC, ChosenPC : STD_LOGIC_VECTOR(pc_width - 1 downto 0);
+	signal PC, NextPC : STD_LOGIC_VECTOR(pc_width - 1 downto 0);
 begin
 	InstrPC <= PC;
 	-- PC Logic
-	pcreg: registry generic map(pc_width) port map(ChosenPC, Clk, Reset, '1', PC);
-	nextpcresult: addersubtractor generic map(pc_width) port map(PC, (0 => '1', others => '0'), '0', NextPC);
-	pcoverridemux: mux2 generic map(pc_width) port map(NextPC, PCOverride, Override, ChosenPC);
+	pcreg: registry generic map(pc_width) port map(NextPC, Clk, Reset, PCEnable, PC);
+	nextpcresult: addersubtractor generic map(pc_width) port map(PC, (0 => '1', others => '0'), Direction, NextPC);
 	-- Decoding Logic
 	instrdecod: instrdecoder port map(Instr, Action, Memory, Operation);
 end;
